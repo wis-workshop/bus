59393b8a9d9ccbada152348a92ae25a30aafc0f2
wis-workshop <workshop@waimeaint.school.nz>
2026-09-18 18:19:04 +1200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "bus-loader-v1";
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE).then(cache =>
+cache.addAll([
+"./",
+"./index.html"
+])
+)
+);
+});
+ 
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(response =>
+response || fetch(event.request)
+)
+);
+});
